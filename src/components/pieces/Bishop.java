229a92701fd7bae76ce4d05efe3652abbdb4bb1d
= <=>
2017-07-26 16:55:32 -0400

    Added Out of Bounds Checks for the Bishop

--- src/components/pieces/Bishop.java
+++ src/components/pieces/Bishop.java
@@ -27,31 +27,32 @@ public class Bishop extends Piece
             if(row - check < 0) checkUpwardsDiagonal = false;
             if(row + check >= board[0].length) checkDownwardsDiagonal = false;
 
-            if(checkUpwardsDiagonal)
+            if(col - check >= 0)
             {
-                //If the up-left diagonal is clear, add the move
-                //Otherwise, check if it is an enemy, and add it and stop checking diagonals
-                //Otherwise, stop checking diagonals and do not add it
-                if(board[col - check][row - check] == null) possibleMoves.add(new int[] {col - check, row - check});
-                else if(canTake(board[col - check][row - check]))
+                if(checkUpwardsDiagonal)
                 {
-                    possibleMoves.add(new int[] {col - check, row - check});
-                    checkUpwardsDiagonal = false;
+                    //If the up-left diagonal is clear, add the move
+                    //Otherwise, check if it is an enemy, and add it and stop checking diagonals
+                    //Otherwise, stop checking diagonals and do not add it
+                    if(board[col - check][row - check] == null) possibleMoves.add(new int[]{col - check, row - check});
+                    else if(canTake(board[col - check][row - check]))
+                    {
+                        possibleMoves.add(new int[]{col - check, row - check});
+                        checkUpwardsDiagonal = false;
+                    } else checkUpwardsDiagonal = false;
                 }
-                else checkUpwardsDiagonal = false;
-            }
-            if(checkDownwardsDiagonal)
-            {
-                //If the down-left diagonal is clear, add the move
-                //Otherwise, check if it is an enemy, and add it and stop checking diagonals
-                //Otherwise, stop checking diagonals and do not add it
-                if(board[col - check][row + check] == null) possibleMoves.add(new int[] {col - check, row + check});
-                else if(canTake(board[col - check][row + check]))
+                if(checkDownwardsDiagonal)
                 {
-                    possibleMoves.add(new int[] {col - check, row + check});
-                    checkDownwardsDiagonal = false;
+                    //If the down-left diagonal is clear, add the move
+                    //Otherwise, check if it is an enemy, and add it and stop checking diagonals
+                    //Otherwise, stop checking diagonals and do not add it
+                    if(board[col - check][row + check] == null) possibleMoves.add(new int[]{col - check, row + check});
+                    else if(canTake(board[col - check][row + check]))
+                    {
+                        possibleMoves.add(new int[]{col - check, row + check});
+                        checkDownwardsDiagonal = false;
+                    } else checkDownwardsDiagonal = false;
                 }
-                else checkDownwardsDiagonal = false;
             }
 
             if(!(checkUpwardsDiagonal || checkDownwardsDiagonal)) break;
@@ -68,31 +69,32 @@ public class Bishop extends Piece
             if(row - check < 0) checkUpwardsDiagonal = false;
             if(row + check >= board[0].length) checkDownwardsDiagonal = false;
 
-            if(checkUpwardsDiagonal)
+            if(col + check < board.length)
             {
-                //If the up-right diagonal is clear, add the move
-                //Otherwise, check if it is an enemy, and add it and stop checking diagonals
-                //Otherwise, stop checking diagonals and do not add it
-                if(board[col + check][row - check] == null) possibleMoves.add(new int[] {col + check, row - check});
-                else if(canTake(board[col + check][row - check]))
+                if(checkUpwardsDiagonal)
                 {
-                    possibleMoves.add(new int[] {col + check, row - check});
-                    checkUpwardsDiagonal = false;
+                    //If the up-right diagonal is clear, add the move
+                    //Otherwise, check if it is an enemy, and add it and stop checking diagonals
+                    //Otherwise, stop checking diagonals and do not add it
+                    if(board[col + check][row - check] == null) possibleMoves.add(new int[]{col + check, row - check});
+                    else if(canTake(board[col + check][row - check]))
+                    {
+                        possibleMoves.add(new int[]{col + check, row - check});
+                        checkUpwardsDiagonal = false;
+                    } else checkUpwardsDiagonal = false;
                 }
-                else checkUpwardsDiagonal = false;
-            }
-            if(checkDownwardsDiagonal)
-            {
-                //If the down-right diagonal is clear, add the move
-                //Otherwise, check if it is an enemy, and add it and stop checking diagonals
-                //Otherwise, stop checking diagonals and do not add it
-                if(board[col + check][row + check] == null) possibleMoves.add(new int[] {col + check, row + check});
-                else if(canTake(board[col + check][row + check]))
+                if(checkDownwardsDiagonal)
                 {
-                    possibleMoves.add(new int[] {col + check, row + check});
-                    checkDownwardsDiagonal = false;
+                    //If the down-right diagonal is clear, add the move
+                    //Otherwise, check if it is an enemy, and add it and stop checking diagonals
+                    //Otherwise, stop checking diagonals and do not add it
+                    if(board[col + check][row + check] == null) possibleMoves.add(new int[]{col + check, row + check});
+                    else if(canTake(board[col + check][row + check]))
+                    {
+                        possibleMoves.add(new int[]{col + check, row + check});
+                        checkDownwardsDiagonal = false;
+                    } else checkDownwardsDiagonal = false;
                 }
-                else checkDownwardsDiagonal = false;
             }
 
             if(!(checkUpwardsDiagonal || checkDownwardsDiagonal)) break;
